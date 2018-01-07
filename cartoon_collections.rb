4bcba416f2f69e6359eab4d3fe7f680f7f31c40b
Helen Du <helendu.614@gmail.com>
2018-01-07 18:38:59 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
-  planeteer_calls.map! do |name|
+  planeteer_calls.map! do |call|
     "{call.capitalize}!"
   end
 end
